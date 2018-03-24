0ed040cfa7c6523e09edb0f669e8b5a652b5d044
Michael Thorsen <bitandia@gmail.com>
2018-03-24 15:29:39 -0700

    Added more buttons for additional features

--- lib/spcontroler.py
+++ lib/spcontroler.py
@@ -6,3 +6,18 @@ def click_power_on():
 
 def click_power_off():
     pyautogui.click(380,450)
+
+def input_video():
+    pyautogui.click(465,435)
+
+def input_s_video():
+    pyautogui.click(685,435)
+
+def input_comp_1():
+    pyautogui.click(465,485)
+
+def input_comp_2():
+    pyautogui.click(685,485)
+
+
+click_power_on()
